924ec77aafe54ab4173354ccf891f0726363eb56
VISHAL M K <132979509+visaluser@users.noreply.github.com>
2025-12-10 20:16:46 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World 2');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
