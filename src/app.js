f1721c8c0e60c70e3092ac09ff61390160e2d60c
VISHAL M K <132979509+visaluser@users.noreply.github.com>
2025-12-11 20:02:26 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Welcome to app.visops.online');
 });
 
 const PORT = process.env.PORT || 3000;
